4b56eb713866ee978d4bfe9a81eaa70ee131161f
lugonesnicolas <lugones.nicolas@gmail.com>
2023-04-19 10:20:47 -0300

    Ejercicio convercion

--- loggs_I/ejercicio1.py
+++ loggs_I/ejercicio1.py
@@ -1,11 +1,21 @@
 #ejercicio1.py
 import logging
 
+"""Logs en un archivo app.log"""
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename='result.log',
+    filemode='w',
+    format='%(name)s - %(levelname)s - %(message)s'
+)
+
+#Lista a trabajar
 fruits = ['Frutilla','Melón','PERA',99.6,'NaranJA', 'mORa', 'NisPERo',99]
 
+#Recorremos y si es posible convertir a minuscula mostramos
 for i in fruits:
     if type(i)==str:
-        # print(type(i))
-        print(i,'se puede convertir')
+        minuscula=i.lower()
+        logging.debug("Se convirtio correctamente %s --> %s", i, minuscula)
     else:
-        print(i,'No se puede convertir')
+        logging.error("No se pudo convertir %s")
