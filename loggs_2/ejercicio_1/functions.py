e9518760ff6ecce5b5d2811c3afdbc347741ffa4
lugonesnicolas <lugones.nicolas@gmail.com>
2023-04-20 15:41:45 -0300

    Optimizado de funcion

--- loggs_2/ejercicio_1/functions.py
+++ loggs_2/ejercicio_1/functions.py
@@ -1,23 +1,26 @@
 import logging
 import logging.config
 
+#Import file.conf
 logging.config.fileConfig('log_config_file.conf')
 
-#Create logger
-logger=logging.getLogger('functions')
+#Create loggers
+logger_functions=logging.getLogger('functions')
+logger_main=logging.getLogger('main')
 
 
-def contar_renglones(cuento):
-    nombre=cuento.name
-    renglones=0
-    for i in cuento:
-        renglones+= 1
-    logger.info('%s - Cantidad de renglones: %s', nombre, renglones) #Logs renglon palabras
-
-
-def contar_lineas_palabras(cuento):
-    renglon=0
-    for i in cuento: #Recorre las lineas
-        palabras=len(i.split()) #Palabras por renglon
-        logger.info('Renglon %s: %s palabras', renglon, palabras) #Logs renglon palabras
-        renglon+= 1
+"""Contador cuenta renglones y palabras
+cuento: TextIOWrapper
+return: None
+"""        
+def contador(cuento):
+    nombre=cuento.name #Nombre del cuento
+    contenido=cuento.read() #Convertimos a string
+    renglones_list=contenido.split("\n") #Separamos los renglones en listas
+    renglones=len(renglones_list) #Contamos los renglones
+    logger_functions.info('%s - Cantidad de renglones: %s', nombre, renglones) #Log cantidad de renglones
+    contador=0 #Contador de renglones para encadenar la cantidad de palabras
+    for i in renglones_list: #Recorremos los renglones que estan en la lista
+        palabras=len(i.split(" ")) #Leemos la cantidad de palabras
+        logger_functions.info('Renglon %s: %s palabras', contador, palabras) #Logs renglon y su cantidad de palabras
+        contador+=1
--- loggs_2/ejercicio_1/main.py
+++ loggs_2/ejercicio_1/main.py
@@ -1,24 +1,15 @@
-import logging
-import logging.config
 import functions as func
 
-logging.config.fileConfig('log_config_file.conf')
-
-#Create logger
-logger=logging.getLogger('main')
-
-
-def main():
+def main(cuento):
     try:
-        logger.info('...Leyendo el archivo...')
-        with open ('cuento.txt', 'r') as f: #Abrimos el archivo
-            func.contar_renglones(f) #Contamos los renglones
-        with open ('cuento.txt', 'r') as f: #Abrimos el archivo
+        func.logger_main.info('...Leyendo el archivo...')
+        with open (cuento, 'r') as f: #Abrimos el archivo
+            func.contador(f) #Contamos los renglones
             func.contar_lineas_palabras(f) #Contamos las palabras por cada renglon
-        logger.info('...Archivo procesado...')
+        func.logger_main.info('...Archivo procesado...')
     
     except ValueError:
-        logger.error('No se pudo abrir el archivo')
+        func.logger_main.error('No se pudo abrir el archivo')
         
 if __name__=="__main__":
-    main()
+    main('cuento.txt')
