2fbd7165ae4dc1fdabff6246cf949f2b2e8f068a
lugonesnicolas <lugones.nicolas@gmail.com>
2023-04-20 15:49:41 -0300

    Proceso en 1 sola funcion

--- loggs_2/ejercicio_1/main.py
+++ loggs_2/ejercicio_1/main.py
@@ -5,7 +5,7 @@ def main(cuento):
         func.logger_main.info('...Leyendo el archivo...')
         with open (cuento, 'r') as f: #Abrimos el archivo
             func.contador(f) #Contamos los renglones
-            func.contar_lineas_palabras(f) #Contamos las palabras por cada renglon
+            # func.contar_lineas_palabras(f) #Contamos las palabras por cada renglon
         func.logger_main.info('...Archivo procesado...')
     
     except ValueError:
